7cf6b57ef0dff60df85015106a40b695885611ab
asianParadissseee <asian.paradissseee@gmail.com>
2023-09-14 12:48:21 +0600

    4. add React and scss loaders

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import {someFn} from "./test";
-
-someFn(10)
-
-
-document.body.innerHTML = `<h1>hello world</h1>`
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import {render} from "react-dom";
+import React from "react";
+import "./components/index.scss"
+
+render(
+    <div>
+    </div>,
+    document.getElementById("root")
+)
