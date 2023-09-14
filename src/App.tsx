e020da1a3f1a001f3a24c1f964ab3d9643ca27d7
asianParadissseee <asian.paradissseee@gmail.com>
2023-09-14 15:39:00 +0600

    6. add routing and code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {Suspense} from 'react';
+import {Link, Route, Routes} from "react-router-dom"
 import "./index.scss"
+import {AboutPageAsync} from "./pages/about-page/about-page.async";
+import {MainPageAsync} from "./pages/main-page/main-page.async";
 
 const App = () => {
     return (
         <div className="app">
-            lorem2
+            <Link to={"/about"}>About</Link>
+            <Link to={"/"}>Home</Link>
+            <Suspense fallback={<div>....loading</div>}>
+
+                <Routes>
+                    <Route path={"/about"} element={<AboutPageAsync/>}/>
+                    <Route path={"/"} element={<MainPageAsync/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
